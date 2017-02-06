81e5a9cb36692978e05b600dddf3e87d5b01891b
ertebilisim <ertebilisim@gmail.com>
2017-02-06 09:02:41 +0200

    Add files via upload

---
+++ alici/alici.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(9, 10); // radio(CE, CS);
+const uint64_t kod = 0xE8E8F0F0E101;
+int sayi[1];
+
+Servo servo;
+int servoPin = 3;
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(1, kod);
+  radio.startListening();
+  servo.attach(servoPin);
+}
+
+void loop() {
+  if (radio.available()) {
+    if ( radio.read(sayi, 1))
+    {
+      servo.write(sayi[0]);
+      Serial.println(sayi[0]);
+    }
+  }
+}
---
+++ verici/verici.ino
@@ -0,0 +1,45 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(9, 10); //radio(CE, CS);
+const uint64_t kod = 0xE8E8F0F0E101;
+int sayi[1];
+int pot;
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(kod);
+
+}
+
+void loop()
+{
+  if (Serial.read())
+  {
+    pot = analogRead(A0);
+    sayi[0] = map(pot, 0, 1023, 0, 180);
+    
+
+    /*
+    int deger;
+    double sicaklik;
+    deger = analogRead(0);
+    sicaklik = Termistor(deger);
+    sayi[0] = sicaklik;
+    */
+    
+    radio.write(sayi, 1);
+    Serial.println(sayi[0]);
+  }
+}
+
+double Termistor(int analogOkuma)
+{
+  double sicaklik;
+  sicaklik = log((10240000 / analogOkuma) - 10000);
+  sicaklik = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * sicaklik * sicaklik)) * sicaklik);
+  sicaklik = sicaklik - 273.15;
+  return sicaklik;
+}
